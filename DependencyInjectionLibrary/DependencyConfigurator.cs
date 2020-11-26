0bba64ca93c69a871b118cf1f41eda41a1b04d93
AngelSel <angelbox1@gmail.com>
2020-11-26 18:20:40 +0300

    Dependencies registration added

--- DependencyInjectionLibrary/DependencyConfigurator.cs
+++ DependencyInjectionLibrary/DependencyConfigurator.cs
@@ -9,13 +9,28 @@ namespace DependencyInjectionLibrary
             where TInterface:class 
             where TImplementation:TInterface
         {
-
+            Register(typeof(TInterface), typeof(TImplementation));
         }
 
 
         public void Register(Type tInterface, Type tImplementation, Configurator.Lifetime lifetime = Configurator.Lifetime.Instance)
         {
+            if(CanCreate(tInterface,tImplementation))
+            {
 
+                Configurator configurator = new Configurator(tInterface, tImplementation, lifetime);
+
+                
+            }
+
+        }
+
+        private bool CanCreate(Type tInterface, Type tImplementation)
+        {
+            if (!tImplementation.IsInterface && !tImplementation.IsAbstract && (tInterface.IsAssignableFrom(tImplementation) || tInterface.IsGenericTypeDefinition))
+                return true;
+            else
+                return false;
         }
 
     }
